01099ef564b8f3bde60f5b0da670b7c36f8acb8b
飛田大二郎 <pelo.canvas@gmail.com>
2021-04-20 23:52:10 +0900

    update first

--- first.php
+++ first.php
@@ -1,8 +1,24 @@
 <?php
 
-$link = mysql_connect('localhost', 'root', '');
+// $dsn = 'mysql: dbname=mini_blog; host=192.168.3.50';
+// $user = 'pelo';
+// $password = '0819Tobita';
 
+// $addr = $_SERVER["REMOTE_ADDR"];
+// echo 'addr='.$addr.'<br />'."\n";
 
+// try
+// {
+//     $con = new PDO($dsn, $user, $password);
+//     echo 'アクセス成功';
+// }
+// catch(PDOException $e)
+// {
+//     echo 'アクセス失敗 ' .$e->getMessage();
+//     exit;
+// }
+
+$str = 'pelo';
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
