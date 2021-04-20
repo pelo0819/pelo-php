0920a458e55865de630e59df99c65be3a5e772ad
飛田大二郎 <pelo.canvas@gmail.com>
2021-04-21 00:00:33 +0900

    update

--- first.php
+++ first.php
@@ -1,24 +1,25 @@
 <?php
 
-// $dsn = 'mysql: dbname=mini_blog; host=192.168.3.50';
-// $user = 'pelo';
-// $password = '0819Tobita';
+$dsn = 'mysql: dbname=mini_blog; host=192.168.3.50';
+$user = 'pelo';
+$password = '0819Tobita';
 
-// $addr = $_SERVER["REMOTE_ADDR"];
-// echo 'addr='.$addr.'<br />'."\n";
-
-// try
-// {
-//     $con = new PDO($dsn, $user, $password);
-//     echo 'アクセス成功';
-// }
-// catch(PDOException $e)
-// {
-//     echo 'アクセス失敗 ' .$e->getMessage();
-//     exit;
-// }
-
+$addr = $_SERVER["REMOTE_ADDR"];
+echo 'addr='.$addr.'<br />'."\n";
 $str = 'pelo';
+try
+{
+    $con = new PDO($dsn, $user, $password);
+    echo 'アクセス成功';
+    $str = 'success';
+}
+catch(PDOException $e)
+{
+    echo 'アクセス失敗 ' .$e->getMessage();
+    $str = 'falied'; 
+    exit;
+}
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
