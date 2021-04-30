467eed8a2341d4336b6e397ed4c33d30f5d8df12
飛田大二郎 <pelo.canvas@gmail.com>
2021-04-30 10:50:58 +0900

    update router

--- framework/core/Router.php
+++ framework/core/Router.php
@@ -9,10 +9,33 @@ class Router
         $this->routes = $this->complieRoutes($definition);
     }
 
-
+    // '/account' 
+    //      => array('controller' => 'account', 'action' => 'index'),
+    // '/account/:action' 
+    //      => array('controller' => 'account')
+    // が引数($definition)で入っている
     public function complieRoutes($definition)
     {
+        $routes = array();
 
+        foreach($definition as $url => $params)
+        {
+            // /で区切る 型はリスト
+            $tokens = explode('/', ltrim($url, '/'));
+            foreach($tokens as $i => $token)
+            {
+                if(0 === strpos($token, ':'))
+                {
+                    $name = substr($token, 1);
+                    $token = '(?P<' . $name .'>[^/]+)';
+                }
+                $tokens[$i] = $token;
+            }
+            $pattern = '/' . implode('/', $tokens);
+            $routes[$pattern] = $params;
+        }
+
+        return $routes;
     }
 
 
